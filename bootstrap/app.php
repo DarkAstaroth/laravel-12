1910b2288b5d39e5989b62e6bde0977f7325e6c9
Victor Manuel Ninahuanca Ayala <victor.ninahuanca@agetic.gob.bo>
2025-07-18 13:15:50 -0400

    chore: add cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -17,6 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+    })
+    ->withMiddleware(function (Middleware $middleware) {
+        // Register native CORS middleware in Laravel 12
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+
+        // You can add other global middleware here
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
